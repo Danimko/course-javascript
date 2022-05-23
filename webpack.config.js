7dc6527f2dba9e1594ac6a53d4fc10fc71b83b9f
Danimko <extaz.showroom@yandex.ru>
2022-05-23 16:26:11 +0300

    Выполнил задание 7

--- webpack.config.js
+++ webpack.config.js
@@ -51,14 +51,26 @@ module.exports = {
         options: { cacheDirectory: true },
       },
       {
-        test: /projects\/.+\.html/,
+        test: /\.html/,
+        include: [path.resolve(__dirname, 'projects')],
         use: [
-          { loader: './scripts/html-inject-loader.js' },
+          {
+            loader: './scripts/html-inject-loader.js',
+          },
           {
             loader: 'raw-loader',
           },
         ],
       },
+      // {
+      //   test: /projects\/.+\.html/,
+      //   use: [
+      //     { loader: './scripts/html-inject-loader.js' },
+      //     {
+      //       loader: 'raw-loader',
+      //     },
+      //   ],
+      // },
       {
         test: /\.(jpe?g|png|gif|svg|eot|ttf|woff|woff2)$/i,
         loader: 'file-loader',
